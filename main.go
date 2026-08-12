ee4d7ed3c06e15e2890861827c9427f6b07431f3
LAPTOP-4V0UT6U6\Acer <gundash1@gmail.com>
2026-08-12 21:38:15 +0700

    check data type

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ func main() {
 	const name string = "GP"
 	// name = "GunP"
 	var age int = 28
-	var salary float32 = 28000
+	var salary float32 = 28000.00
 	var aboveAvg bool = true
 	fmt.Println("Hello World")
 	fmt.Println(name, age)
 	fmt.Println("salary ", salary)
 	fmt.Println("above avg ", aboveAvg)
+	fmt.Printf("value %v\n", salary)
+	fmt.Printf("Data Type %T\n", salary)
 }
