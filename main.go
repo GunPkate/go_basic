d82a86aaf64da1e1214c571f7bdd448fc21b7edc
LAPTOP-4V0UT6U6\Acer <gundash1@gmail.com>
2026-08-12 21:30:28 +0700

    variable var const int float32 string bool

--- main.go
+++ main.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	const name string = "GP"
+	// name = "GunP"
+	var age int = 28
+	var salary float32 = 28000
+	var aboveAvg bool = true
 	fmt.Println("Hello World")
-	fmt.Println("GP")
+	fmt.Println(name, age)
+	fmt.Println("salary ", salary)
+	fmt.Println("above avg ", aboveAvg)
 }
